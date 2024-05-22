d434f7d438f877748c5e5df3f3f314cbe326edfa
* <*>
2024-05-22 17:35:49 -0300

    Feature: Preparando arquivos
    Preparando ambiente para começar a implementar as funções controller de User.

--- backend/src/controllers/UserControllers/CreateUserController.ts
+++ backend/src/controllers/UserControllers/CreateUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { IUserService } from '../@types'
+import { IUserService } from '../../@types'
 
 export const CreateUserController = ( UserService: IUserService ) => {
 
