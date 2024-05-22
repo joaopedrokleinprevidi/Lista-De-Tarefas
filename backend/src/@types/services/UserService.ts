9cbf5d41648c47ef44ddb8ae724add2900ac6c26
MBrayanS <micael.brayan.s@gmail.com>
2024-05-22 00:32:07 -0300

    FEAT: Criando o método GetUserByEmail
    Responsável por retornar um usuário por email, caso ele exista.

--- backend/src/@types/services/UserService.ts
+++ backend/src/@types/services/UserService.ts
@@ -2,4 +2,5 @@ import { UserRecord } from "firebase-admin/auth"
 
 export interface IUserService {
     createUser: ( email: string, password: string ) => Promise<UserRecord>
+    getUserByEmail: ( email: string ) => Promise<UserRecord>
 }
