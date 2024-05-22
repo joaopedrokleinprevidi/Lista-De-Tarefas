17b66d819664a760a42dd8894315e005aa6e49c7
* <*>
2024-05-22 15:40:25 -0300

    Feature: ToAssignTaskToCategory

---
+++ backend/src/controllers/CategoryControllers/ToAssignTaskToCategory.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { ICategoryModel } from '../../@types'
+
+export const toAssignTaskToCategoryController = ( CategoryModel: ICategoryModel) => {
+    
+    const execute = async ( request: Request, response: Response ) => {
+
+        try {
+            const { userID, categoryID, taskID } = request.body;
+            await CategoryModel.toAssignTaskToCategory(userID, categoryID, taskID)
+
+            response.status(200).json({ message: "Task sucessfully added to category." })
+        }
+    
+        catch ( error: any ) {
+            response.status(500).json(error)
+        }
+
+    }
+
+    return execute
+
+}
