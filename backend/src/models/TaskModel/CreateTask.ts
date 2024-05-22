b1402ddd43d1300f5300cb4ae4da2dca3459e377
MBrayanS <micael.brayan.s@gmail.com>
2024-05-22 00:23:38 -0300

    FEAT: Adicionando retorno ao createTask
    Agora quando uma tarefa for criada com sucesso este método irá retornar um objeto com os dados da tarefa.

--- backend/src/models/TaskModel/CreateTask.ts
+++ backend/src/models/TaskModel/CreateTask.ts
@@ -4,7 +4,10 @@ import { ITaskModel } from "../../@types"
 export const createTask: ITaskModel["createTask"] = async ( userID, task ) => {
 
     try {
-        await database.collection("Usuarios").doc(userID).collection("Tarefas").add(task)  
+        const newTask = await database.collection("Usuarios").doc(userID).collection("Tarefas").add(task)
+        const newTaskDoc = await newTask.get()
+
+        return { id: newTaskDoc.id, ...newTaskDoc.data() }
     }
 
     catch ( error: any ) {
