e97b5eda7d2d0f3cfebbb77908ba7d67326b31bb
MBrayanS <micael.brayan.s@gmail.com>
2024-05-22 00:35:56 -0300

    FEAT: Criando o método deleteUser
    Responsável por deletar um usuário através de um id, caso ele exista.

--- backend/src/@types/services/UserService.ts
+++ backend/src/@types/services/UserService.ts
@@ -3,4 +3,5 @@ import { UserRecord } from "firebase-admin/auth"
 export interface IUserService {
     createUser: ( email: string, password: string ) => Promise<UserRecord>
     getUserByEmail: ( email: string ) => Promise<UserRecord>
+    deleteUser: ( userID: string ) => Promise<void>
 }
--- backend/src/services/firebaseAdminService/index.ts
+++ backend/src/services/firebaseAdminService/index.ts
@@ -1,2 +1,3 @@
 export * from "./CreateUser"
 export * from "./GetUserByEmail"
+export * from "./DeleteUser"
