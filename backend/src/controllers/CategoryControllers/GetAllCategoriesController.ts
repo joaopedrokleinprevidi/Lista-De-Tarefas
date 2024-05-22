a1ee11e8cc29333ee29b94c867db1f4429f8ed52
* <*>
2024-05-22 15:40:04 -0300

    Feature: GetAllCategoriesController

---
+++ backend/src/controllers/CategoryControllers/GetAllCategoriesController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { ICategoryModel } from '../../@types'
+
+export const GetAllCategoriesController = ( CategoryModel: ICategoryModel) => {
+    
+    const execute = async ( request: Request, response: Response ) => {
+
+        try {
+            const { userID } = request.body;
+            const allCategories = await CategoryModel.getAllCategories(userID)
+
+            response.status(200).json(allCategories)
+        }
+    
+        catch ( error: any ) {
+            response.status(500).json(error)
+        }
+
+    }
+
+    return execute
+
+}
