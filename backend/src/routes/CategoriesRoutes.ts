a0d6850274a35613b0cf79a0e35efd9001772a2b
João Pedro Klein Previdi <kleincoder@gmail.com>
2024-05-23 18:48:49 -0300

    Refactor: CategoriesRoutes

--- backend/src/routes/CategoriesRoutes.ts
+++ backend/src/routes/CategoriesRoutes.ts
@@ -1,17 +1,21 @@
+import express from 'express'
+const categoriesRoutes = express.Router() 
+
 import { AuthMiddleware } from '../middlewares/authMiddleware'
 import { CreateCategoryController, DeleteCategoryController, GetAllCategoriesController, ToAssignTaskToCategoryController, ToRemoveTaskFromCategoryController, GetCategoryController, updateCategory } from '../modules/controllers/CategoriesControllers'
-import router from './index'
 
-router.post("/categories/create", AuthMiddleware.VerifyUser, CreateCategoryController)
+categoriesRoutes.post("/categories/create", AuthMiddleware.VerifyUser, CreateCategoryController)
 
-router.delete("/categories/delete", AuthMiddleware.VerifyUser, DeleteCategoryController)
+categoriesRoutes.delete("/categories/delete", AuthMiddleware.VerifyUser, DeleteCategoryController)
 
-router.get("/categories/getAll", AuthMiddleware.VerifyUser, GetAllCategoriesController)
+categoriesRoutes.get("/categories/getAll", AuthMiddleware.VerifyUser, GetAllCategoriesController)
 
-router.get("/categories/get", AuthMiddleware.VerifyUser, GetCategoryController)
+categoriesRoutes.get("/categories/get", AuthMiddleware.VerifyUser, GetCategoryController)
 
-router.put("/categories/assignTaskToCategory", AuthMiddleware.VerifyUser, ToAssignTaskToCategoryController)
+categoriesRoutes.put("/categories/assignTaskToCategory", AuthMiddleware.VerifyUser, ToAssignTaskToCategoryController)
 
-router.put("/categories/removeTaskFromCategory", AuthMiddleware.VerifyUser, ToRemoveTaskFromCategoryController)
+categoriesRoutes.put("/categories/removeTaskFromCategory", AuthMiddleware.VerifyUser, ToRemoveTaskFromCategoryController)
 
-router.put("/categories/update", AuthMiddleware.VerifyUser, updateCategory)
+categoriesRoutes.put("/categories/update", AuthMiddleware.VerifyUser, updateCategory)
+
+export default categoriesRoutes
