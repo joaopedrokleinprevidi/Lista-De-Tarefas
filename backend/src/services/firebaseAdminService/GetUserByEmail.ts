4332580913b6be3fcfd1b411a961c0c10c2fff92
MBrayanS <micael.brayan.s@gmail.com>
2024-05-25 23:00:03 -0300

    FEAT: Tratando erros em getUserByEmail
    Aplicado uma estrutura try/catch e um retorno mais protegido ao método.

--- backend/src/services/firebaseAdminService/GetUserByEmail.ts
+++ backend/src/services/firebaseAdminService/GetUserByEmail.ts
@@ -1,6 +1,15 @@
 import { getAuth } from "firebase-admin/auth"
 import { IUserService } from "../../@types/services/UserService"
+import { IUser } from "../../@types/domain/User"
 
 export const getUserByEmail: IUserService["getUserByEmail"] = async ( email: string ) => {
-    return await getAuth().getUserByEmail(email)
+    try {
+        const user = await getAuth().getUserByEmail(email) as IUser
+
+        return { uid: user.uid, email: user.email }
+    }
+
+    catch ( error: any ) {
+        throw error
+    }
 }
