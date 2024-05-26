e1718e14130281cdadf1bcbcd3ab91b1b55883d9
MBrayanS <micael.brayan.s@gmail.com>
2024-05-25 22:58:42 -0300

    FEAT: Tratando erros em createUser
    Aplicado um tratamento de erros para o método a partir do adapter FirebaseAuthErrorsAdapter.
    
    Um retorno mais protegido também foi adicionado ao método.

--- backend/src/services/firebaseAdminService/CreateUser.ts
+++ backend/src/services/firebaseAdminService/CreateUser.ts
@@ -1,6 +1,18 @@
-import { getAuth } from "firebase-admin/auth"
+import { FirebaseAuthError, getAuth } from "firebase-admin/auth"
+import { FirebaseAuthErrorsAdapter } from "../../adapters/firebase/FirebaseAuthErrorsAdapter"
 import { IUserService } from "../../@types"
 
 export const createUser: IUserService["createUser"] = async ( email: string, password: string ) => {
-    return await getAuth().createUser({ email, password })
+
+    try {
+        const newUser = await getAuth().createUser({ email, password })
+
+        return { uid: newUser.uid, email: newUser.providerData[0].email }
+    }
+    
+    catch ( error: any ) {
+        if( error instanceof FirebaseAuthError ) FirebaseAuthErrorsAdapter(error)
+
+        throw error
+    }
 }
