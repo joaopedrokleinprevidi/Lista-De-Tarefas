a8233f205048c5c34bb7c01fe2e6289a0359c3f3
MBrayanS <micael.brayan.s@gmail.com>
2024-06-13 10:32:41 -0300

    FEAT: Lista de status para requisições
    Neste arquivo está os códigos de resposta para requisições da aplicação.

---
+++ backend/src/errors/statusCodeHttps.ts
@@ -0,0 +1,9 @@
+export const OK_CODE = 200
+export const CREATED_CODE = 201
+export const NO_CONTENT_CODE = 204
+export const BAD_REQUEST_CODE = 400
+export const UNAUTHORIZED_CODE = 401
+export const FORBIDDEN_CODE = 403
+export const NOT_FOUND_CODE = 404
+export const CONFLICT_CODE = 409
+export const INTERNAL_SERVER_ERROR_CODE = 500
